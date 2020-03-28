314f20efb285727f6daef2e389a79e29c94a41c9
kun <kun@localhost.localdomain>
2020-03-28 09:18:20 +0800

    initial commiit

--- src/tests/traverse_memory.cc
+++ src/tests/traverse_memory.cc
@@ -4,6 +4,11 @@
 int main(void)
 {
     puts("hello traverse memory");
+    MallocExtension::instance()->VerifyAllMemory();
     size_t s = MallocExtension::instance()->GetThreadCacheSize();
+
+    char buf[10240] = {0};
+    MallocExtension::instance()->GetStats(buf, 10240);
+    puts(buf);
     return 0;
 }
